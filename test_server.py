8d2491435a3d23563f04c32c577c9f2d9e81d6f2
Mihirjoshii <87995531+Mihirjoshii@users.noreply.github.com>
2024-07-03 23:27:37 +0530

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/auth/success', methods=['GET'])
+@app.route('/auth/success', methods=['GET','POST','PUT'])
 def auth_success():
     return jsonify({"message": "Authentication successful!"}), 200
 
